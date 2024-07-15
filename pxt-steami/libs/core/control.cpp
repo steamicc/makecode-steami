e92d6030698a413dd9485b18df9b61d3eee429b4
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2024-07-15 15:58:24 +0200

    complete core package

--- pxt-steami/libs/core/control.cpp
+++ pxt-steami/libs/core/control.cpp
@@ -1,11 +1,68 @@
 #include "pxt.h"
 
 #if defined(NRF52_SERIES) || defined(PICO_BOARD)
-#define _estack __StackTop 
+#define _estack __StackTop
 #endif
 extern uint32_t _estack;
 
+enum EventFlags {
+    //%
+    QueueIfBusy = MESSAGE_BUS_LISTENER_QUEUE_IF_BUSY,
+    //%
+    DropIfBusy = MESSAGE_BUS_LISTENER_DROP_IF_BUSY,
+    //%
+    Reentrant = MESSAGE_BUS_LISTENER_REENTRANT
+};
+
+
 namespace control {
+
+/**
+ * Gets the number of milliseconds elapsed since power on.
+ */
+//% help=control/millis weight=50
+//% blockId=control_running_time block="millis (ms)"
+int millis() {
+    return system_timer_current_time();
+}
+
+/**
+ * Gets current time in microseconds. Overflows every ~18 minutes.
+ */
+//%
+int micros() {
+    return system_timer_current_time_us() & 0x3fffffff;
+}
+
+/**
+ * Schedules code that run in the background.
+ */
+//% help=control/in-background blockAllowMultiple=1 afterOnStart=true
+//% blockId="control_in_background" block="run in background" blockGap=8
+void inBackground(Action a) {
+    runInParallel(a);
+}
+
+/**
+ * Registers an event handler.
+ */
+//% weight=20 blockGap=8 blockId="control_on_event" block="on event|from
+//% src=control_event_source_id|with value %value=control_event_value_id" % help=control/on-event %
+//% blockExternalInputs=1
+void onEvent(int src, int value, Action handler, int flags = 0) {
+    if (!flags)
+        flags = ::EventFlags::QueueIfBusy;
+    registerWithDal(src, value, handler, (int)flags);
+}
+
+/**
+ * Blocks the calling thread until the specified event is raised.
+ */
+//% help=control/wait-for-event async
+//% blockId=control_wait_for_event block="wait for event|from %src|with value %value"
+void waitForEvent(int src, int value) {
+    pxt::waitForEvent(src, value);
+}
 
 /**
  * Announce that an event happened to registered handlers.
@@ -20,8 +77,28 @@ void raiseEvent(int src, int value) {
 }
 
 /**
-* Determine the version of system software currently running.
-*/
+ * Gets the value of the last event executed on the bus
+ */
+//% blockId=control_event_value" block="event value"
+//% help=control/event-value
+//% weight=18
+int eventValue() {
+    return pxt::lastEvent.value;
+}
+
+/**
+ * Gets the timestamp of the last event executed on the bus
+ */
+//% blockId=control_event_timestamp" block="event timestamp"
+//% help=control/event-timestamp
+//% weight=19 blockGap=8
+int eventTimestamp() {
+    return pxt::lastEvent.timestamp;
+}
+
+/**
+ * Determine the version of system software currently running.
+ */
 //% blockId="control_device_dal_version" block="device dal version"
 //% help=control/device-dal-version
 String deviceDalVersion() {
@@ -29,11 +106,21 @@ String deviceDalVersion() {
 }
 
 /**
-* Allocates the next user notification event
-*/
+ * Allocates the next user notification event
+ */
 //% help=control/allocate-notify-event
 int allocateNotifyEvent() {
     return ::allocateNotifyEvent();
+}
+
+/**
+ *
+ */
+//%
+void __log(int priority, String text) {
+    if (NULL == text)
+        return;
+    pxt::sendSerial(text->getUTF8Data(), text->getUTF8Size());
 }
 
 /** Write a message to DMESG debugging buffer. */
@@ -49,9 +136,8 @@ void dmesgPtr(String str, Object_ ptr) {
 }
 
 //%
-uint32_t _ramSize()
-{
+uint32_t _ramSize() {
     return (uint32_t)&_estack & 0x1fffffff;
 }
 
-}
+} // namespace control
--- pxt-steami/libs/core/shims.d.ts
+++ pxt-steami/libs/core/shims.d.ts
@@ -2,6 +2,41 @@
 declare namespace control {
 
     /**
+     * Gets the number of milliseconds elapsed since power on.
+     */
+    //% help=control/millis weight=50
+    //% blockId=control_running_time block="millis (ms)" shim=control::millis
+    function millis(): int32;
+
+    /**
+     * Gets current time in microseconds. Overflows every ~18 minutes.
+     */
+    //% shim=control::micros
+    function micros(): int32;
+
+    /**
+     * Schedules code that run in the background.
+     */
+    //% help=control/in-background blockAllowMultiple=1 afterOnStart=true
+    //% blockId="control_in_background" block="run in background" blockGap=8 shim=control::inBackground
+    function inBackground(a: () => void): void;
+
+    /**
+     * Registers an event handler.
+     */
+    //% weight=20 blockGap=8 blockId="control_on_event" block="on event|from
+    //% src=control_event_source_id|with value %value=control_event_value_id" % help=control/on-event %
+    //% blockExternalInputs=1 flags.defl=0 shim=control::onEvent
+    function onEvent(src: int32, value: int32, handler: () => void, flags?: int32): void;
+
+    /**
+     * Blocks the calling thread until the specified event is raised.
+     */
+    //% help=control/wait-for-event async
+    //% blockId=control_wait_for_event block="wait for event|from %src|with value %value" shim=control::waitForEvent
+    function waitForEvent(src: int32, value: int32): void;
+
+    /**
      * Announce that an event happened to registered handlers.
      * @param src ID of the MicroBit Component that generated the event
      * @param value Component specific code indicating the cause of the event.
@@ -10,6 +45,22 @@ declare namespace control {
     //% help=control/raise-event
     //% block="raise event|from %src|with value %value" blockExternalInputs=1 shim=control::raiseEvent
     function raiseEvent(src: int32, value: int32): void;
+
+    /**
+     * Gets the value of the last event executed on the bus
+     */
+    //% blockId=control_event_value" block="event value"
+    //% help=control/event-value
+    //% weight=18 shim=control::eventValue
+    function eventValue(): int32;
+
+    /**
+     * Gets the timestamp of the last event executed on the bus
+     */
+    //% blockId=control_event_timestamp" block="event timestamp"
+    //% help=control/event-timestamp
+    //% weight=19 blockGap=8 shim=control::eventTimestamp
+    function eventTimestamp(): int32;
 
     /**
      * Determine the version of system software currently running.
@@ -24,6 +75,12 @@ declare namespace control {
     //% help=control/allocate-notify-event shim=control::allocateNotifyEvent
     function allocateNotifyEvent(): int32;
 
+    /**
+     *
+     */
+    //% shim=control::__log
+    function __log(priority: int32, text: string): void;
+
     /** Write a message to DMESG debugging buffer. */
     //% shim=control::dmesg
     function dmesg(s: string): void;
@@ -31,6 +88,38 @@ declare namespace control {
     /** Write a message and value (pointer) to DMESG debugging buffer. */
     //% shim=control::dmesgPtr
     function dmesgPtr(str: string, ptr: Object): void;
+}
+declare namespace control {
+
+    /**
+     * Force GC and dump basic information about heap.
+     */
+    //% shim=control::gc
+    function gc(): void;
+
+    /**
+     * Force GC and halt waiting for debugger to do a full heap dump.
+     */
+    //% shim=control::heapDump
+    function heapDump(): void;
+
+    /**
+     * Set flags used when connecting an external debugger.
+     */
+    //% shim=control::setDebugFlags
+    function setDebugFlags(flags: int32): void;
+
+    /**
+     * Record a heap snapshot to debug memory leaks.
+     */
+    //% shim=control::heapSnapshot
+    function heapSnapshot(): void;
+
+    /**
+     * Return true if profiling is enabled in the current build.
+     */
+    //% shim=control::profilingEnabled
+    function profilingEnabled(): boolean;
 }
 declare namespace pins {
 
@@ -55,6 +144,136 @@ declare namespace pins {
     //% blockId=pins_pulse_duration block="pulse duration (µs)"
     //% weight=19 shim=pins::pulseDuration
     function pulseDuration(): int32;
+}
+
+
+
+    //% indexerGet=BufferMethods::getByte indexerSet=BufferMethods::setByte
+declare interface Buffer {
+    /**
+     * Reads an unsigned byte at a particular location
+     */
+    //% shim=BufferMethods::getUint8
+    getUint8(off: int32): int32;
+
+    /**
+     * Returns false when the buffer can be written to.
+     */
+    //% shim=BufferMethods::isReadOnly
+    isReadOnly(): boolean;
+
+    /**
+     * Writes an unsigned byte at a particular location
+     */
+    //% shim=BufferMethods::setUint8
+    setUint8(off: int32, v: int32): void;
+
+    /**
+     * Write a number in specified format in the buffer.
+     */
+    //% shim=BufferMethods::setNumber
+    setNumber(format: NumberFormat, offset: int32, value: number): void;
+
+    /**
+     * Read a number in specified format from the buffer.
+     */
+    //% shim=BufferMethods::getNumber
+    getNumber(format: NumberFormat, offset: int32): number;
+
+    /** Returns the length of a Buffer object. */
+    //% property shim=BufferMethods::length
+    length: int32;
+
+    /**
+     * Fill (a fragment) of the buffer with given value.
+     */
+    //% offset.defl=0 length.defl=-1 shim=BufferMethods::fill
+    fill(value: int32, offset?: int32, length?: int32): void;
+
+    /**
+     * Return a copy of a fragment of a buffer.
+     */
+    //% offset.defl=0 length.defl=-1 shim=BufferMethods::slice
+    slice(offset?: int32, length?: int32): Buffer;
+
+    /**
+     * Shift buffer left in place, with zero padding.
+     * @param offset number of bytes to shift; use negative value to shift right
+     * @param start start offset in buffer. Default is 0.
+     * @param length number of elements in buffer. If negative, length is set as the buffer length minus
+     * start. eg: -1
+     */
+    //% start.defl=0 length.defl=-1 shim=BufferMethods::shift
+    shift(offset: int32, start?: int32, length?: int32): void;
+
+    /**
+     * Convert a buffer to string assuming UTF8 encoding
+     */
+    //% shim=BufferMethods::toString
+    toString(): string;
+
+    /**
+     * Convert a buffer to its hexadecimal representation.
+     */
+    //% shim=BufferMethods::toHex
+    toHex(): string;
+
+    /**
+     * Rotate buffer left in place.
+     * @param offset number of bytes to shift; use negative value to shift right
+     * @param start start offset in buffer. Default is 0.
+     * @param length number of elements in buffer. If negative, length is set as the buffer length minus
+     * start. eg: -1
+     */
+    //% start.defl=0 length.defl=-1 shim=BufferMethods::rotate
+    rotate(offset: int32, start?: int32, length?: int32): void;
+
+    /**
+     * Write contents of `src` at `dstOffset` in current buffer.
+     */
+    //% shim=BufferMethods::write
+    write(dstOffset: int32, src: Buffer): void;
+
+    /**
+     * Compute k-bit FNV-1 non-cryptographic hash of the buffer.
+     */
+    //% shim=BufferMethods::hash
+    hash(bits: int32): uint32;
+}
+declare namespace control {
+
+    /**
+     * Create a new zero-initialized buffer.
+     * @param size number of bytes in the buffer
+     */
+    //% deprecated=1 shim=control::createBuffer
+    function createBuffer(size: int32): Buffer;
+
+    /**
+     * Create a new buffer with UTF8-encoded string
+     * @param str the string to put in the buffer
+     */
+    //% deprecated=1 shim=control::createBufferFromUTF8
+    function createBufferFromUTF8(str: string): Buffer;
+}
+declare namespace loops {
+
+    /**
+     * Repeats the code forever in the background. On each iteration, allows other codes to run.
+     * @param body code to execute
+     */
+    //% help=loops/forever weight=100 afterOnStart=true deprecated=true
+    //% blockId=forever_deprecated block="forever" blockAllowMultiple=1 shim=loops::forever
+    function forever(a: () => void): void;
+
+    /**
+     * Pause for the specified time in milliseconds
+     * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
+     */
+    //% help=loops/pause weight=99 deprecated=true
+    //% async block="pause %pause=timePicker|ms"
+    //% blockId=device_pause_deprecated shim=loops::pause
+    function pause(ms: int32): void;
 }
 declare namespace control {
 
