073fcb452454aedcdd38a8f962a268840efcd5e2
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2024-10-10 14:24:41 +0000

    feat: :construction_worker: Add Conventions Verification Tools and Automation

---
+++ git-precommit-checks.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  display: {
+    notifications: true,
+    offendingContent: true,
+    rulesSummary: false,
+    shortStats: true,
+    verbose: false,
+  },
+  rules: [
+    {
+      message: 'Aurais-tu oublié de terminer certaines tâches ?  Aurais-tu une issue à ouvrir pour traiter cette tache plus tard ?',
+      nonBlocking: true,
+      regex: /(?:FIXME|TODO)/,
+    },
+    {
+      message: 'Tu as des marqueurs de conflits qui traînent dans ton code',
+      regex: /^[<>|=]{4,}/m,
+    },
+    {
+      message:
+        'Arrêt du commit : tu as renseigné des choses qui ne doivent pas être commitées !',
+      regex: /do not commit/i,
+    },
+    {
+      filter: /\.js$/,
+      message: '🤔 Hum ! N’as-tu pas oublié de retirer du "console.log(…)" ?',
+      nonBlocking: true,
+      regex: /^\s*console\.log/,
+    },
+  ],
+}
