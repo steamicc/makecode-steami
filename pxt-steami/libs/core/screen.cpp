0c7de8d3158c102f4381b0d03f3b1aa6acbb6023
Charly-sketch <glic.per@gmail.com>
2024-10-23 08:10:31 +0000

    feat : main blocks simulation

--- pxt-steami/libs/core/screen.cpp
+++ pxt-steami/libs/core/screen.cpp
@@ -1,12 +1,39 @@
 //% block="Screen" weight=100 color=#f57e00 icon=""
 namespace screen {
 /**
- * Draws a pixel on the screen at the specified coordinates with the given color.
+ * @brief a pixel on the screen at the specified coordinates with the given color.
  * @param x the x-coordinate of the pixel, eg: 0
  * @param y the y-coordinate of the pixel, eg: 0
  */
-//% block="draw pixel at x $x y $y color $color"
-//% x.min=0
-//% y.min=0
+//% block="draw pixel at x $x y $y"
+//% help=screen/drawPixel
+//% blockId="screen_drawPixel" blockGap=8
+//% x.min=0 x.max=130
+//% y.min=0 y.max=130
 void drawPixel(int x, int y) {}
+
+/**
+ * @brief Draw a line on the screen from the specified coordinates to the specified coordinates with
+ * the given color.
+ * @param x0 the x-coordinate of the start of the line, eg: 0
+ * @param y0 the y-coordinate of the start of the line, eg: 0
+ * @param x1 the x-coordinate of the end of the line, eg: 0
+ * @param y1 the y-coordinate of the end of the line, eg: 0
+ */
+//% block="draw line from x $x0 y $y0 to x $x1 y $y1"
+//% help=screen/drawLine
+//% blockId="screen_drawLine" blockGap=8
+//% x0.min=0 x0.max=130
+//% y0.min=0 y0.max=130
+//% x1.min=0 x1.max=130
+//% y1.min=0 y1.max=130
+void drawLine(int x0, int y0, int x1, int y1) {}
+
+/**
+ * @brief Clear the screen
+ */
+//% block="Clear the screen" weight=80
+//% help=screen/clearScreen
+//% blockId="screen_clearScreen" blockGap=8
+void clearScreen() {}
 } // namespace screen
--- pxt-steami/libs/screen/sim/screen.ts
+++ pxt-steami/libs/screen/sim/screen.ts
@@ -5,5 +5,36 @@ namespace pxsim.screen {
         BTN_MENU = 3,
     }
 
-    export function drawPixel(x: number, y: number) {}
+    export function drawPixel(x: number, y: number) {
+        let screenState = pxsim.screenSteamiState();
+        let screen = screenState.getState();
+        let pixel = screen.find(p => p.x === x && p.y === y);
+        if (pixel) {
+            pixel.on = true;
+        }
+        screenState.setState(screen);
+        runtime.queueDisplayUpdate();
+    }
+
+    export function drawLine(x1: number, y1: number, x2: number, y2: number) {
+        let screenState = pxsim.screenSteamiState();
+        let screen = screenState.getState();
+        let dx = Math.abs(x2 - x1);
+        let dy = Math.abs(y2 - y1);
+        let sx = x1 < x2 ? 1 : -1;
+        let sy = y1 < y2 ? 1 : -1;
+        let err = dx - dy;
+
+        while (true) {}
+    }
+
+    export function clearScreen() {
+        let screenState = pxsim.screenSteamiState();
+        let screen = screenState.getState();
+        screen.forEach(pixel => {
+            pixel.on = false;
+        });
+        screenState.setState(screen);
+        runtime.queueDisplayUpdate();
+    }
 }
