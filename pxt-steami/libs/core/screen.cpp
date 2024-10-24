fd131fb1a726fe08446919d0e46142410ae42cdc
Charly-sketch <glic.per@gmail.com>
2024-10-24 08:11:05 +0000

    feat : draw blocks simulation
    line, rectangle and circle

--- pxt-steami/libs/core/screen.cpp
+++ pxt-steami/libs/core/screen.cpp
@@ -10,24 +10,65 @@ namespace screen {
 //% blockId="screen_drawPixel" blockGap=8
 //% x.min=0 x.max=130
 //% y.min=0 y.max=130
+//% weight=100
+//% group="Draw"
 void drawPixel(int x, int y) {}
 
 /**
- * @brief Draw a line on the screen from the specified coordinates to the specified coordinates with
- * the given color.
+ * @brief Draw a line on the screen from the specified coordinates to the specified coordinates.
  * @param x0 the x-coordinate of the start of the line, eg: 0
  * @param y0 the y-coordinate of the start of the line, eg: 0
- * @param x1 the x-coordinate of the end of the line, eg: 0
- * @param y1 the y-coordinate of the end of the line, eg: 0
+ * @param x1 the x-coordinate of the end of the line, eg: 128
+ * @param y1 the y-coordinate of the end of the line, eg: 128
  */
-//% block="draw line from x $x0 y $y0 to x $x1 y $y1"
+//% block="draw line from (x0: $x0, y0: $y0) to (x1: $x1, y1: $y1)"
 //% help=screen/drawLine
 //% blockId="screen_drawLine" blockGap=8
-//% x0.min=0 x0.max=130
-//% y0.min=0 y0.max=130
-//% x1.min=0 x1.max=130
-//% y1.min=0 y1.max=130
+//% x0.min=0 x0.max=128
+//% y0.min=0 y0.max=128
+//% x1.min=0 x1.max=128
+//% y1.min=0 y1.max=128
+//% inlineInputMode=inline
+//% weight=90
+//% group="Draw"
 void drawLine(int x0, int y0, int x1, int y1) {}
+
+/**
+ * @brief Draw a rectangle on the screen at the specified coordinates with the specified width and
+ * height.
+ * @param x the x-coordinate of the top-left corner of the rectangle, eg: 0
+ * @param y the y-coordinate of the top-left corner of the rectangle, eg: 0
+ * @param width the width of the rectangle, eg: 128
+ * @param height the height of the rectangle, eg: 128
+ */
+//% block="draw rectangle at (x $x, y $y) of width $width and height $height"
+//% help=screen/drawRectangle
+//% blockId="screen_drawRectangle" blockGap=8
+//% x.min=0 x.max=128
+//% y.min=0 y.max=128
+//% width.min=0 width.max=128
+//% height.min=0 height.max=128
+//% inlineInputMode=inline
+//% weight=80
+//% group="Draw"
+void drawRectangle(int x, int y, int width, int height) {}
+
+/**
+ * @brief Draw a circle on the screen at the specified coordinates with the specified radius.
+ * @param x0 the x-coordinate of the center of the circle, eg: 64
+ * @param y0 the y-coordinate of the center of the circle, eg: 64
+ * @param r the radius of the circle, eg: 64
+ */
+//% block="draw circle at (x $x0 y $y0) of radius $r"
+//% help=screen/drawCircle
+//% blockId="screen_drawCircle" blockGap=8
+//% x0.min=0 x0.max=128
+//% y0.min=0 y0.max=128
+//% r.min=0 r.max=128
+//% inlineInputMode=inline
+//% weight=70
+//% group="Draw"
+void drawCircle(int x0, int y0, int r) {}
 
 /**
  * @brief Clear the screen
@@ -35,5 +76,6 @@ void drawLine(int x0, int y0, int x1, int y1) {}
 //% block="Clear the screen" weight=80
 //% help=screen/clearScreen
 //% blockId="screen_clearScreen" blockGap=8
+//% group="Basic"
 void clearScreen() {}
 } // namespace screen
--- pxt-steami/libs/screen/sim/screen.ts
+++ pxt-steami/libs/screen/sim/screen.ts
@@ -6,33 +6,84 @@ namespace pxsim.screen {
     }
 
     export function drawPixel(x: number, y: number) {
-        let screenState = pxsim.screenSteamiState();
-        let screen = screenState.getState();
+        let screen = pxsim.screenSteamiState().getState();
         let pixel = screen.find(p => p.x === x && p.y === y);
         if (pixel) {
             pixel.on = true;
         }
-        screenState.setState(screen);
+        pxsim.screenSteamiState().setState(screen);
         runtime.queueDisplayUpdate();
     }
 
     export function drawLine(x1: number, y1: number, x2: number, y2: number) {
-        let screenState = pxsim.screenSteamiState();
-        let screen = screenState.getState();
         let dx = Math.abs(x2 - x1);
         let dy = Math.abs(y2 - y1);
         let sx = x1 < x2 ? 1 : -1;
         let sy = y1 < y2 ? 1 : -1;
         let err = dx - dy;
+
+        let screen = pxsim.screenSteamiState().getState();
+
+        while (true) {
+            let pixel = screen.find(p => p.x === x1 && p.y === y1);
+            if (pixel) pixel.on = true;
+
+            if (x1 === x2 && y1 === y2) break;
+
+            let e2 = err * 2;
+            if (e2 > -dy) {
+                err -= dy;
+                x1 += sx;
+            }
+            if (e2 < dx) {
+                err += dx;
+                y1 += sy;
+            }
+        }
+
+        pxsim.screenSteamiState().setState(screen);
+        runtime.queueDisplayUpdate();
+    }
+
+    export function drawRectangle(
+        x: number,
+        y: number,
+        width: number,
+        height: number,
+    ) {
+        let screen = pxsim.screenSteamiState().getState();
+        for (let i = x; i < x + width; i++) {
+            for (let j = y; j < y + height; j++) {
+                let pixel = screen.find(p => p.x === i && p.y === j);
+                if (pixel) pixel.on = true;
+            }
+        }
+        pxsim.screenSteamiState().setState(screen);
+        runtime.queueDisplayUpdate();
+    }
+
+    export function drawCircle(x: number, y: number, radius: number) {
+        let screen = pxsim.screenSteamiState().getState();
+        for (let i = -radius; i <= radius; i++) {
+            for (let j = -radius; j <= radius; j++) {
+                if (i * i + j * j <= radius * radius) {
+                    let pixel = screen.find(
+                        px => px.x === x + i && px.y === y + j,
+                    );
+                    if (pixel) pixel.on = true;
+                }
+            }
+        }
+        pxsim.screenSteamiState().setState(screen);
+        pxsim.runtime.queueDisplayUpdate();
     }
 
     export function clearScreen() {
-        let screenState = pxsim.screenSteamiState();
-        let screen = screenState.getState();
+        let screen = pxsim.screenSteamiState().getState();
         screen.forEach(pixel => {
             pixel.on = false;
         });
-        screenState.setState(screen);
+        pxsim.screenSteamiState().setState(screen);
         runtime.queueDisplayUpdate();
     }
 }
